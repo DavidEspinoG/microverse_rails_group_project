f8cfb01e2adabc64b4f5739d2f07480eb5619e0a
johnie261 <njorogejohn261@gmail.com>
2023-06-26 22:51:59 +0300

    Generate migration for food and user models

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+user = User.create(name: 'Tom')
+
+food = Food.create(
+    name: 'Apple',
+    measuring_unit: 'piece',
+    price: 1.58,
+    quantity: 5,
+    user: user
+)
