69c07651a41fa90cc14ac657fbb2932707794862
David Espino <davidespinog@gmail.com>
2023-06-26 15:06:50 -0600

    Solve merge issues

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
- 
-ActiveRecord::Schema[7.0].define(version: 2023_06_26_181019) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_26_183330) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,7 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_26_181019) do
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_foods_on_user_id"
   end
-  
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -38,5 +37,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_26_181019) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
   add_foreign_key "foods", "users"
 end
