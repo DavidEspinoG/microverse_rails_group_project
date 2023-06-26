4acc76e63603bc32b5343caf43cb23db3712db30
David Espino <davidespinog@gmail.com>
2023-06-26 16:57:22 -0600

    Add view for new recipe

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,23 @@
+class RecipesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @recipes = Recipe.all
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new name: params[:name], preparation_time: params[:preparation_time],
+            cooking_time: params[:cooking_time], description: params[:description],
+            public: params[:public], user: current_user
+    if @recipe.save
+      redirect_to '/'
+    else 
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   root "home#index"
-
+  resources :recipes, except: [:delete]
   resources :foods, only: [:index, :new, :create]
 
 end
