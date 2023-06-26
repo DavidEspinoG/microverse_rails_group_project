5a54247d2fcddde508a0142e330934c7f93aac4a
David Espino <davidespinog@gmail.com>
2023-06-26 16:58:17 -0600

    Fix rubocop offenses

--- Gemfile
+++ Gemfile
@@ -54,9 +54,6 @@ group :development, :test do
   gem 'capybara', '~> 3.39', '>= 3.39.2'
   gem 'debug', platforms: %i[mri mingw x64_mingw]
   gem 'rspec-rails', '~> 6.0', '>= 6.0.3'
-
-  gem 'debug', platforms: %i[mri mingw x64_mingw]
-
 end
 
 group :development do
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -11,13 +11,12 @@ class RecipesController < ApplicationController
 
   def create
     @recipe = Recipe.new name: params[:name], preparation_time: params[:preparation_time],
-            cooking_time: params[:cooking_time], description: params[:description],
-            public: params[:public], user: current_user
+                         cooking_time: params[:cooking_time], description: params[:description],
+                         public: params[:public], user: current_user
     if @recipe.save
       redirect_to '/'
-    else 
+    else
       render :new, status: :unprocessable_entity
     end
   end
-
 end
