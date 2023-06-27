a551164a52047c98bcdbbe5b61f7181e4a43c9d3
David Espino <davidespinog@gmail.com>
2023-06-27 15:48:34 -0600

    Add ingredient#new action and view

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -4,4 +4,14 @@ class IngredientsController < ApplicationController
     @recipe = Recipe.find(params[:recipe_id])
     @food = Food.new
   end
+
+  def create
+    @food = Food.new(name: params[:name], 
+      quantity: params[:quantity], user: current_user)
+    @recipe = Recipe.find(params[:recipe_id])
+    if @food.save
+      @recipe.foods.push(@food)
+      redirect_to recipe_path(@recipe)
+    end
+  end
 end
