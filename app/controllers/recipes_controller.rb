aa34b1cfc32d1da4e9359c0df62ea7984796e314
David Espino <davidespinog@gmail.com>
2023-06-27 09:15:00 -0600

    Add user recipes view

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,7 +2,7 @@ class RecipesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @recipes = Recipe.all
+    @recipes = current_user.recipes
   end
 
   def new
@@ -14,9 +14,16 @@ class RecipesController < ApplicationController
                          cooking_time: params[:cooking_time], description: params[:description],
                          public: params[:public], user: current_user
     if @recipe.save
-      redirect_to '/'
+      redirect_to '/recipes'
     else
       render :new, status: :unprocessable_entity
     end
   end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to '/recipes'
+  end
+
 end
