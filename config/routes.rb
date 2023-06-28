f27da0f3c500c542206b82dc684747167905697b
johnie261 <njorogejohn261@gmail.com>
2023-06-28 23:41:40 +0300

    Configire routes for shopping_list

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   get 'ingredients/new/:recipe_id', to: 'ingredients#new', as: 'new_ingredient'
   post 'ingredients/new/:recipe_id', to: 'ingredients#create', as: 'create_ingredient'
   resources :public_recipes, only: [:index]
+  resources :general_shopping_list
 end
