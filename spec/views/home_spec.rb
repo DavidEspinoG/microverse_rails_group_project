ba07cc23cbce44f589d29dd6645f96c351e46c4d
David Espino <davidespinog@gmail.com>
2023-06-28 10:14:26 -0600

    Add tests for home view

--- spec/views/home_spec.rb
+++ spec/views/home_spec.rb
@@ -6,5 +6,23 @@ RSpec.describe 'Home view', type: :system do
       visit '/'
       expect(page).to have_content('Log in')
     end
+    it 'should show sign up button' do 
+      visit '/'
+      expect(page).to have_content('Sign up')
+    end
+    it 'should show the title' do 
+      visit '/'
+      expect(page).to have_content('Recipe app')
+    end
+  end
+  context 'When the user is authenticated' do 
+    let!(:user) {User.create name: 'David', email: 'david@mail.com', password: 'password'}
+    it 'should show the users name' do 
+      visit '/users/sign_in'
+      fill_in('Email', with: user.email)
+      fill_in('Password', with: user.password)
+      find_button('Log in').click
+      expect(page).to have_content('David')
+    end
   end
 end
