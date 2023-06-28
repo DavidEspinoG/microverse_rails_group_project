9fd4ae97a8b1cae5ee87a9d4870014dad2be02f2
johnie261 <njorogejohn261@gmail.com>
2023-06-29 00:29:28 +0300

    Edit index ation

--- app/controllers/general_shopping_list_controller.rb
+++ app/controllers/general_shopping_list_controller.rb
@@ -5,10 +5,16 @@ class GeneralShoppingListController < ApplicationController
     @general_food = @user.foods
 
     @missing_foods = []
+    @total_cost = 0
+
     @recipes.each do |recipe|
       missing_foods = recipe.foods - @general_food
-      @missing_foods.push(missing_foods)
+      @missing_foods.concat(missing_foods)
+    end
+
+    @missing_foods.flatten.each do |food|
+      total = food.quantity * food.price
+      @total_cost += total
     end
   end
-
 end
