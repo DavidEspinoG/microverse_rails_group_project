068adc64631597105760c6fca395b3aecfee055f
David Espino <davidespinog@gmail.com>
2023-06-28 16:20:19 -0600

    Reorganize folders on spec directory

--- spec/foods/new_spec.rb
+++ spec/foods/new_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe '/foods/new path', type: :system do 
-  let!(:user) { User.create name: 'David', email: 'david@mail.com', password: 'password'}
-  it 'should show the user\'s name' do 
+RSpec.describe '/foods/new path', type: :system do
+  let!(:user) { User.create name: 'David', email: 'david@mail.com', password: 'password' }
+  it 'should show the user\'s name' do
     login_as(user)
     visit new_food_path
     expect(page).to have_content(user.name)
   end
-  it 'should show food name when new food is created' do 
+  it 'should show food name when new food is created' do
     login_as(user)
     visit new_food_path
     fill_in('Name', with: 'Carrot')
